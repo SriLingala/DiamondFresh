f7197d4518f846ad0cd7ecfef154d10090e315d5
sri.lingala <srilingala1@gmail.com>
2025-03-01 08:13:41 +0000

    Initial Commit

---
+++ DiamondFresh/DiamondFreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiamondFreshApp.swift
+//  DiamondFresh
+//
+//  Created by Sri Ramanjaneyulu Lingala on 01/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct DiamondFreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
